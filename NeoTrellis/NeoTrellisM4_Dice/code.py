84b568009cb562729d3c8e1ccbf006d52aca15bc
Zach Metcalf <zachery.metcalf@gmail.com>
2026-02-23 21:02:54 -0700

    Set white around dice

--- NeoTrellis/NeoTrellisM4_Dice/code.py
+++ NeoTrellis/NeoTrellisM4_Dice/code.py
@@ -17,7 +17,7 @@ All text above must be included in any redistribution.
 """
 
 """
-No serious, you should give Adafruit your money. Lady Ada rocks!
+No seriously, you should give Adafruit your money. Lady Ada rocks!
 Copyright (c) 2026 Zach Metcalf
 Licensed under the MIT license.
 All text above must be included in any redistribution.
@@ -36,20 +36,20 @@ import busio
 # Set up the trellis and accelerometer
 
 trellis = adafruit_trellism4.TrellisM4Express()
-trellis.pixels.brightness = 0.1
+trellis.pixels.brightness = 0.05
 
 i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 
 number_patterns = [
-    ["*  ", " * ", "  *", "   "],  # 0 # Just for animation
-    ["   ", " * ", "   ", "   "],  # 1
-    ["*  ", "   ", "  *", "   "],  # 2
-    ["*  ", " * ", "  *", "   "],  # 3
-    ["* *", "   ", "* *", "   "],  # 4
-    ["* *", " * ", "* *", "   "],  # 5
-    ["***", "   ", "***", "   "],  # 6
+    ["*  ", " * ", "  *"],  # 0 # Just for animation
+    ["   ", " * ", "   "],  # 1
+    ["*  ", "   ", "  *"],  # 2
+    ["*  ", " * ", "  *"],  # 3
+    ["* *", "   ", "* *"],  # 4
+    ["* *", " * ", "* *"],  # 5
+    ["***", "   ", "***"],  # 6
 ]
 
 
@@ -60,36 +60,24 @@ def display_digit(number, offset, color):
     color      -- the RGB color to use to display the digit
     force_zero -- whether to leave a 0 blank (False) or display it
     """
-    print("displaying", number, "at offset", offset, "with color", color)
     time.sleep(0.1)
     bits = number_patterns[number]
-    for row in range(4):
+    for row in range(3):
         for col in range(3):
             if bits[row][col] == " " or number == 0:
-                trellis.pixels[col + offset, row] = (0, 0, 0)
+                trellis.pixels[col + offset, row] = (255, 255, 255)
             else:
                 trellis.pixels[col + offset, row] = color
 
 
 def display_number(numbers, color, position=0):
-    """Display a multi-digit number.
-    If the number is > 99, the hundreds digit (1-4) is indicated on the far left column:
-        100 is the top pixel, 400 is the bottom.
-    number      -- the number to display
-    color       -- the RGB color to use to display the digit
-    force_zeros -- whether to leave a 0 in the tens place blank (False) or display it
-    """
     display_digit(numbers[0], 5, color)
     display_digit(numbers[1], 1, color)
-    for h in range(4):
+    for h in range(3):
         trellis.pixels[0, h] = (0, 0, 0)
 
 
 def animate_to(numbers, color=(255, 0, 0)):
-    """Perform an animation (displaying random numbers) before displaying the requested number.
-    numbers -- the numbers to eventually display
-    color  -- the color to use (indicates the type of dice used)
-    """
     for _ in range(6):
         trellis.pixels.fill((0, 0, 0))
         display_number([random.randint(1, 6), random.randint(1, 6)], color, 0)
@@ -99,11 +87,6 @@ def animate_to(numbers, color=(255, 0, 0)):
 
 
 def roll(sides):
-    """Generate a random dice roll.
-    Returns the total of the roll.
-    number -- the number of dice to roll
-    sides  -- the number of side on dice to roll (4, 6, 8, 10, 12, 20)
-    """
     return [random.randint(1, sides), random.randint(1, sides)]
 
 
@@ -112,11 +95,6 @@ bound = 4.0
 
 
 def shaken():
-    """Detect when the Trellis is shaken.
-    See http://www.profoundlogic.com/docs/display/PUI/Accelerometer+Test+for+Shaking
-    TL;DR one or more axis experiences a significant (set by bound) change very quickly
-    Returns whether a shake was detected.
-    """
     global previous_reading
     result = False
     x, y, z = accelerometer.acceleration
